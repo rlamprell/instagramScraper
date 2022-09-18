b551f0735f6df0876b321c7cf48e81ab9f181d4e
rlamprell <rob.lamprell@hotmail.co.uk>
2022-09-18 15:39:41 +0100

    removing dead code

--- insta_followers.py
+++ insta_followers.py
@@ -9,16 +9,13 @@ class instagramAPI:
     url = "https://i.instagram.com/api/v1/users/web_profile_info"
     payload = ""
     headers = {
-        # "cookie": "csrftoken=ZgPs7y0GqW5VspLv2BBi1pOJvYMZviEA; rur=%22RVA%5C05455117407996%5C0541695040786%3A01f71d9f6280b930e943391416b8b70f164697f9c49a695f5b4b732a77b817a58fb7b697%22; ds_user_id=55117407996",
         "Accept": "*/*",
         "Accept-Language": "en-GB,en;q=0.9",
         "Accept-Encoding": "gzip, deflate, br",
         "Host": "i.instagram.com",
         "Origin": "https://www.instagram.com",
-        # "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.1 Safari/605.1.15",
         "Referer": "https://www.instagram.com/",
         "Connection": "keep-alive",
-        # "Cookie": "_js_datr=9BAnY590VPcz3K6z7_NHHMxz; dpr=2; csrftoken=ZgPs7y0GqW5VspLv2BBi1pOJvYMZviEA; ds_user_id=55117407996; rur="RVA\05455117407996\0541695040628:01f73845f530a4b34c5fffe887966e6be6e95487d5ca03d1931fba6e6abe5b9b9fa01f77"; sessionid=55117407996%3A9XHE0yTle5Xhtt%3A5%3AAYeoHZCLvpkRH2U_KclBi7oxOOSqjEmO8iOOLndj9A; ig_did=1A26F683-F5F9-46E1-B578-AEB143FF3D88; mid=YoeGggAEAAHCGs-7STo2LbPn3kMm",
         "X-ASBD-ID": "198387",
         "X-IG-App-ID": "936619743392459",
         "Priority": "u=3, i",
